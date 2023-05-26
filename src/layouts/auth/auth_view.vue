21479530b6f345d372d4e922d120957d690b0675
Anup Shrestha <anupshresth9@gmail.com>
2023-05-26 07:08:04 +0545

    fixed app bar

--- src/layouts/auth/auth_view.vue
+++ src/layouts/auth/auth_view.vue
@@ -24,39 +24,37 @@ function railDrawer(rail) {
 </script>
 
 <template>
-  <v-layout>
-    <v-app-bar color="primary" rail>
-      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
-      <v-app-bar-title>Legends</v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon="mdi-logout" @click="logout"></v-btn>
-    </v-app-bar>
+  <v-app-bar color="primary">
+    <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+    <v-app-bar-title>Legends</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon="mdi-logout" @click="logout"></v-btn>
+  </v-app-bar>
 
-    <v-navigation-drawer elevation="2" v-model="toggle" :rail="rail">
-      <v-list-item
-        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-        title="Admin"
-        subtitle="admin@gmail.com"
-        nav
-        @click="railDrawer(false)"
-      >
-        <template v-slot:append>
-          <v-btn
-            variant="text"
-            icon="mdi-chevron-left"
-            @click.stop="railDrawer(true)"
-          ></v-btn>
-        </template>
-      </v-list-item>
+  <v-navigation-drawer elevation="2" v-model="toggle" :rail="rail">
+    <v-list-item
+      prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+      title="Admin"
+      subtitle="admin@gmail.com"
+      nav
+      @click="railDrawer(false)"
+    >
+      <template v-slot:append>
+        <v-btn
+          variant="text"
+          icon="mdi-chevron-left"
+          @click.stop="railDrawer(true)"
+        ></v-btn>
+      </template>
+    </v-list-item>
 
-      <v-divider></v-divider>
-      <auth-sidebar></auth-sidebar>
-    </v-navigation-drawer>
-    <v-main>
-      <v-container>
-        <router-view />
-        <status-message></status-message>
-      </v-container>
-    </v-main>
-  </v-layout>
+    <v-divider></v-divider>
+    <auth-sidebar></auth-sidebar>
+  </v-navigation-drawer>
+  <v-main>
+    <v-container>
+      <router-view />
+      <status-message></status-message>
+    </v-container>
+  </v-main>
 </template>
